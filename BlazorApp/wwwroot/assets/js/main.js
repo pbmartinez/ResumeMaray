879620bcb38f2fa733323d010f7699e8e287d87e
Pablo B. Martinez Pedroso <pmartinezpedroso@gmail.com>
2025-01-26 18:15:54 -0300

    add about page

--- BlazorApp/wwwroot/assets/js/main.js
+++ BlazorApp/wwwroot/assets/js/main.js
@@ -55,46 +55,46 @@
   }
   window.addEventListener('load', aosInit);
 
-  /**
-   * Animate the skills items on reveal
-   */
-  let skillsAnimation = document.querySelectorAll('.skills-animation');
-  skillsAnimation.forEach((item) => {
-    new Waypoint({
-      element: item,
-      offset: '80%',
-      handler: function(direction) {
-        let progress = item.querySelectorAll('.progress .progress-bar');
-        progress.forEach(el => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%';
-        });
-      }
-    });
-  });
+  ///**
+  // * Animate the skills items on reveal
+  // */
+  //let skillsAnimation = document.querySelectorAll('.skills-animation');
+  //skillsAnimation.forEach((item) => {
+  //  new Waypoint({
+  //    element: item,
+  //    offset: '80%',
+  //    handler: function(direction) {
+  //      let progress = item.querySelectorAll('.progress .progress-bar');
+  //      progress.forEach(el => {
+  //        el.style.width = el.getAttribute('aria-valuenow') + '%';
+  //      });
+  //    }
+  //  });
+  //});
 
-  /**
-   * Initiate Pure Counter
-   */
-  new PureCounter();
+  ///**
+  // * Initiate Pure Counter
+  // */
+  //new PureCounter();
 
-  /**
-   * Init swiper sliders
-   */
-  function initSwiper() {
-    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
-      let config = JSON.parse(
-        swiperElement.querySelector(".swiper-config").innerHTML.trim()
-      );
+  ///**
+  // * Init swiper sliders
+  // */
+  //function initSwiper() {
+  //  document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+  //    let config = JSON.parse(
+  //      swiperElement.querySelector(".swiper-config").innerHTML.trim()
+  //    );
 
-      if (swiperElement.classList.contains("swiper-tab")) {
-        initSwiperWithCustomPagination(swiperElement, config);
-      } else {
-        new Swiper(swiperElement, config);
-      }
-    });
-  }
+  //    if (swiperElement.classList.contains("swiper-tab")) {
+  //      initSwiperWithCustomPagination(swiperElement, config);
+  //    } else {
+  //      new Swiper(swiperElement, config);
+  //    }
+  //  });
+  //}
 
-  window.addEventListener("load", initSwiper);
+  //window.addEventListener("load", initSwiper);
 
   ///**
   // * Initiate glightbox
